a63a6def8898dc7137ea445a4ab95e96276b0d8d
rajan-gith <rajan.prajapati@berylsystems.com>
2019-11-16 16:24:26 +0530

    signup component added to router

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './App.css';
 import Login from './components/login/login.js'
+import SignUp from './components/signup/signup.js'
 import Navbar from './components/navbar/navbar.js'
 import {BrowserRouter, Route} from 'react-router-dom';
 
@@ -10,6 +11,7 @@ function App() {
       <BrowserRouter>
         <Route path='/' component = {Navbar}/>
         <Route path="/login" component={Login} />
+        <Route path="/sign_up" component={SignUp} />
       </BrowserRouter>
     </div>
   );
