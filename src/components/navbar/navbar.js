87cf5cffc78e09c8c77d06241c12aa1187caeb8a
rajan-gith <rajan.prajapati@berylsystems.com>
2019-11-19 17:41:47 +0530

    navbar component changes

--- src/components/navbar/navbar.js
+++ src/components/navbar/navbar.js
@@ -27,9 +27,11 @@ export default class Navbar extends Component{
       if (result.status !== 100){
         let path_name = ""
         path_name = this.props.location.pathname
-        if ((path_name === "/sign_up") || (path_name === "login") ){
+        if ((path_name === "/sign_up") || (path_name === "login") || (path_name === "/forgot_password") ){
           this.props.history.push(this.props.location.pathname)
-        }else {
+        }else if (path_name === "/new_password") {
+        }
+        else {
           this.props.history.push('/')
         }
       }
