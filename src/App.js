5543c64698d0fbfee16a0cc5165723638a9e2023
rajan-gith <rajan.prajapati@berylsystems.com>
2019-11-20 14:40:38 +0530

    app.js switch used

--- src/App.js
+++ src/App.js
@@ -5,17 +5,19 @@ import SignUp from './components/signup/signup.js'
 import Navbar from './components/navbar/navbar.js'
 import ForgotPassword from './components/login/forgot_password.js'
 import NewPassword from './components/login/new_password.js'
-import {BrowserRouter, Route} from 'react-router-dom';
+import {BrowserRouter, Route, Switch} from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
         <Route path='/' component = {Navbar}/>
-        <Route path="/login" component={Login} />
-        <Route path="/sign_up" component={SignUp} />
-        <Route path="/forgot_password" component={ForgotPassword} />
-        <Route path="/new_password" component={NewPassword} />
+        <Switch>
+          <Route path="/login" component={Login} />
+          <Route path="/sign_up" component={SignUp} />
+          <Route path="/forgot_password" component={ForgotPassword} />
+          <Route path="/new_password" component={NewPassword} />
+        </Switch>
       </BrowserRouter>
     </div>
   );
