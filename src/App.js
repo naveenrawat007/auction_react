921993102fb20cba4b08ec354d61830e812ee35e
rajan-gith <rajan.prajapati@berylsystems.com>
2019-12-17 16:11:44 +0530

    User new property changes route changes in app file

--- src/App.js
+++ src/App.js
@@ -12,8 +12,7 @@ import AdminSidebar from './components/admin/sidebar.js'
 import NewPassword from './components/login/new_password.js'
 import {BrowserRouter, Route, Switch} from 'react-router-dom';
 import NewProperty from './components/property/new.js';
-
-
+import UserNewProperty from './components/user/property/new.js';
 
 function App() {
   return (
@@ -24,7 +23,7 @@ function App() {
           <Route exact path="/user" component={() => <Sidebar path='user_profile'/>}/>
           <Route exact path="/property/new" component={NewProperty}/>
           <Route exact path="/user/property/" component={() => <Sidebar path='property_list'/>}/>
-          <Route exact path="/user/property/new" component={() => <Sidebar path='new_property'/>}/>
+          <Route exact path="/user/property/new" component={UserNewProperty}/>
           <Route exact path="/user/property/:id" component={PropertyShow}/>
           <Route exact path="/admin" component={() => <AdminSidebar path='all_users_list'/>}/>
           <Route exact path="/admin/property/under_review" component={() => <AdminSidebar path='under_review_property_list'/>}/>
