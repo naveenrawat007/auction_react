6fc72f3aab6209a2bb84a0f1cbe915e2318072ff
rajan-gith <rajan.prajapati@berylsystems.com>
2020-01-13 19:28:45 +0530

    user list page change status modal added

--- src/components/admin/user/user_list.js
+++ src/components/admin/user/user_list.js
@@ -1,12 +1,16 @@
 import React, {Component} from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import {Link} from 'react-router-dom';
+// import {Link} from 'react-router-dom';
+import Modal from 'react-bootstrap/Modal';
 import { faSearch } from '@fortawesome/free-solid-svg-icons';
 const initial_state = {
+  status_modal: false,
+  selected_user: "",
+  selected_status: "",
+  user_status_options: [],
   error: "",
   message: "",
   isLoaded: false,
-  selected_user: "",
   selected_user_token: "",
   users: [],
   search_str: "",
@@ -56,6 +60,7 @@ export default class AllUserList extends Component{
             users: result.users,
             current_page : result.meta.current_page,
             total_pages : result.meta.total_pages,
+            user_status_options: result.statuses,
           });
           let items = []
           for (let number = 1; number <= this.state.total_pages; number++) {
@@ -142,9 +147,69 @@ export default class AllUserList extends Component{
     }, function () {
       this.setState({
         selected_user_token: this.state.users[this.state.selected_user].token ,
+        selected_status: this.state.users[this.state.selected_user].status,
       });
     });
   }
+  updateStatus = () => {
+    this.setState({
+      isLoaded: false,
+    });
+    let url = process.env.REACT_APP_BACKEND_BASE_URL + "/admin/properties/status"
+    const fd = new FormData();
+    fd.append('property[id]', this.state.properties[this.state.selected_property].id)
+    fd.append('property[status]', this.state.selected_status)
+    fd.append('property[termination_reason]', this.state.termination_reason)
+    fd.append('property[auction_started_at]', this.state.auction_started_at)
+    fd.append('property[auction_length]', this.state.auction_length)
+    fetch(url, {
+      method: "PUT",
+      headers: {
+        "Authorization": localStorage.getItem("auction_admin_token"),
+        "Accept": "application/vnd.auction_backend.v1",
+        "Access-Control-Allow-Origin": "*",
+				"Access-Control-Allow-Credentials": "*",
+				"Access-Control-Expose-Headers": "*",
+				"Access-Control-Max-Age": "*",
+				"Access-Control-Allow-Methods": "*",
+				"Access-Control-Allow-Headers": "*"
+      },
+      body: fd,
+    }).then(res => res.json())
+    .then((result) => {
+      if (this._isMounted){
+        this.getPropertiesList();
+        if (result.status === 200){
+          this.setState({
+            message: result.message,
+            variant: "success"
+          });
+          this.clearMessageTimeout = setTimeout(() => {
+            this.setState(() => ({message: ""}))
+          }, 2000);
+        }
+      }
+    })
+  }
+  updateSelectedStatus = (event) => {
+    const{ name, value } = event.target;
+    this.setState({
+      [name]: value
+    });
+  }
+  hideModal = () => {
+    this.setState({
+      status_modal: false,
+      // selected_status: "",
+    });
+  }
+
+  openStatusModal = () => {
+    this.setState({
+      status_modal: true
+    });
+  }
+
 
 	render() {
     const current_page = this.state.current_page;
@@ -168,6 +233,24 @@ export default class AllUserList extends Component{
         </tr>
       );
     })
+    const status_array = this.state.user_status_options.map((status, index) => {
+      return(
+        <li className="list-inline-item" key={index}>
+          <div className="custom-control custom-radio">
+            {
+              this.state.selected_user === ""
+                ?
+                  null
+                :
+                <>
+                  <input type="radio" name="selected_status" value={status} checked={this.state.selected_status === status} id={index+this.state.users.length} className="custom-control-input" onChange={this.updateSelectedStatus} />
+                  <label className="custom-control-label" htmlFor={index+this.state.users.length} >{status}</label>
+                </>
+            }
+          </div>
+        </li>
+      )
+    })
     const pagination = this.state.total_pages_array.map((page, index) => {
       return (
         <button className={this.checkActiveClass(page, current_page)} key={index} onClick={this.refreshList} page_number={page}>{page}</button>
@@ -176,87 +259,95 @@ export default class AllUserList extends Component{
     const prev_page = <> <button className="pagination-btn btn" onClick={this.refreshList} page_number={this.getPreviousPage(current_page)}>Prev</button> </>
     const next_page = <> <button className="pagination-btn btn" onClick={this.refreshList} page_number={this.getNextPage(current_page, total_pages)}>Next</button> </>
 		return (
-      <div id="userList" className="container tab-container px-0 tab-pane active">
+      <div id="allUser" className="container tab-container px-0 tab-pane active">
         <div className="profile-form">
-          <div className="prop-bind">
-            <ul className="nav nav-pills property_tabs px-3">
-              <li className="nav-item">
-                <Link className="nav-link sub-nav-link active" data-toggle="pill" to="/admin">All User</Link>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link sub-nav-link" data-toggle="pill" href="#freeUser">Free User</a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link sub-nav-link" data-toggle="pill" href="#premiumUser">Premium User</a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link sub-nav-link" data-toggle="pill" href="#banUser">Ban User</a>
-              </li>
-            </ul>
-            <div className="tab-content">
-              <div id="allUser" className="container tab-container px-0 tab-pane active">
-                <div className="profile-form">
-                  <div className="profile-form-in prop-bind">
-                    <div className="search-box row mx-0 pb-3">
-                      <div className="col-md-5 px-0">
-                        <div className="input-group">
-                          <input type="text" className="form-control" placeholder="Search..." aria-label="Username" aria-describedby="basic-addon1" name="search_str" onChange={this.searchHandler}/>
-                          <div className="input-group-append">
-                            <span className="input-group-text red-btn" id="basic-addon1">
-                              <FontAwesomeIcon icon={faSearch} size="1x" />
-                            </span>
-                          </div>
-                        </div>
-                      </div>
-                      <div className="col-md-5 offset-md-2 px-0 text-right">
-                        <button className="btn red-btn admin-btns" type="button" onClick={this.viewUser}>View</button>
-                        &nbsp;
-                        <button className="btn red-btn admin-btns" type="button" onClick={this.viewUser}>Edit</button>
-                        &nbsp;
-                        <button className="btn red-btn admin-btns" type="button">Control</button>
-                      </div>
-                    </div>
-                    <div className="under_review admin-review loading-spinner-parent">
-                      <table className="table table-bordered table-hover review_table alluser_table">
-                        <thead>
-                          <tr>
-                            <th></th>
-                            <th>Name</th>
-                            <th>User Status</th>
-                            <th>Subscription</th>
-                            <th>Expiration</th>
-                            <th>Promo Code</th>
-                            <th>User Number</th>
-                            <th>User Email</th>
-                          </tr>
-                        </thead>
-                      </table>
-                      {this.state.isLoaded === true ?
-                        null
-                      :
-                      <div className="spinner_main">
-                        <div className="spinner-grow" role="status">
-                          <span className="sr-only">Loading...</span>
-                        </div>
-                      </div>
-                      }
-                      <div className="under_review_list">
-                        <table className="table table-bordered table-hover review_table alluser_table">
-                          <tbody>
-                            {userList}
-                          </tbody>
-                        </table>
-                      </div>
-                    </div>
-                    <div className="col-md-12 text-center my-3">
-                      {prev_page}{pagination}{next_page}
-                    </div>
+          <div className="profile-form-in prop-bind">
+            <div className="search-box row mx-0 pb-3">
+              <div className="col-md-5 px-0">
+                <div className="input-group">
+                  <input type="text" className="form-control" placeholder="Search..." aria-label="Username" aria-describedby="basic-addon1" name="search_str" onChange={this.searchHandler}/>
+                  <div className="input-group-append">
+                    <span className="input-group-text red-btn" id="basic-addon1">
+                      <FontAwesomeIcon icon={faSearch} size="1x" />
+                    </span>
                   </div>
                 </div>
               </div>
+              <div className="col-md-5 offset-md-2 px-0 text-right">
+                <button className="btn red-btn admin-btns" type="button" onClick={this.viewUser}>View</button>
+                &nbsp;
+                <button className="btn red-btn admin-btns" type="button" onClick={this.viewUser}>Edit</button>
+                &nbsp;
+                <button className="btn red-btn admin-btns" type="button" onClick={this.openStatusModal}>Control</button>
+              </div>
+            </div>
+            <div className="under_review admin-review loading-spinner-parent">
+              <table className="table table-bordered table-hover review_table alluser_table">
+                <thead>
+                  <tr>
+                    <th></th>
+                    <th>Name</th>
+                    <th>User Status</th>
+                    <th>Subscription</th>
+                    <th>Expiration</th>
+                    <th>Promo Code</th>
+                    <th>User Number</th>
+                    <th>User Email</th>
+                  </tr>
+                </thead>
+              </table>
+              {this.state.isLoaded === true ?
+                null
+              :
+              <div className="spinner_main">
+                <div className="spinner-grow" role="status">
+                  <span className="sr-only">Loading...</span>
+                </div>
+              </div>
+              }
+              <div className="under_review_list">
+                <table className="table table-bordered table-hover review_table alluser_table">
+                  <tbody>
+                    {userList}
+                  </tbody>
+                </table>
+              </div>
+            </div>
+            <div className="col-md-12 text-center my-3">
+              {prev_page}{pagination}{next_page}
             </div>
           </div>
         </div>
+        <Modal className="status_modal" show={this.state.status_modal} onHide={this.hideModal}>
+          <Modal.Header closeButton>
+            <div className=" offset-md-1 col-md-10 text-center">
+              <h5 className="mb-0 text-uppercase"> { this.state.selected_user === "" ? "Please select User" : ("Status for " + this.state.users[this.state.selected_user].first_name + this.state.users[this.state.selected_user].last_name )}</h5>
+            </div>
+          </Modal.Header>
+          <div className="modal-body">
+            {this.state.selected_user === ""
+              ?
+                null
+              :
+
+              <div className="row mx-0">
+                <div className="col-md-6 px-0">
+                  <div className="status-list">
+                    <ul className="list-inline">
+                      <form>
+                        {status_array}
+                      </form>
+                    </ul>
+                  </div>
+                </div>
+              </div>
+            }
+            <div className="col-md-12 text-center mt-3">
+              <span className="error"></span>
+              <button type="button" className="btn red-btn btn-default" data-dismiss="modal" onClick={this.updateStatus}>Save</button>
+            </div>
+          </div>
+        </Modal>
       </div>
     );
 	}
