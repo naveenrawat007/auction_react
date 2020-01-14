452801afde5ebfa0109630d319fe3668b6d5e945
rajan-gith <rajan.prajapati@berylsystems.com>
2020-01-14 18:03:27 +0530

    plans index js added

---
+++ src/components/user/plan/index.js
@@ -0,0 +1,27 @@
+import React, {Component} from 'react';
+import {Link} from 'react-router-dom';
+
+export default class Plan extends Component{
+	constructor(props){
+    super(props);
+  }
+
+  componentDidMount () {
+  }
+
+
+	render() {
+		return (
+			<div className="profile-setting">
+        <div className="container custom_container px-0">
+          <div className="row mx-0 profile_row my-5">
+            <div className="col-md-12 px-0">
+              <div className="tab-content">
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		);
+	}
+}
