67e9460e72464d98beb802506eaf8c3a96d68de0
rajan-gith <rajan.prajapati@berylsystems.com>
2020-01-22 14:27:16 +0530

    about and help page added

---
+++ src/components/static/help_sidebar.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react';
+import {Link} from 'react-router-dom';
+
+export default class HelpSidebar extends Component{
+  _isMounted = false
+  render(){
+    return(
+      <div className="container-fluid home_main px-0">
+      </div>
+    )
+  }
+}
