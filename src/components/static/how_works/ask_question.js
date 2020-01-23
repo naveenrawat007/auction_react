06af6b8d6ae2e12a1744b42e8d295d30d3bea4f9
lalit Kumar <lalit.Kumar@berylsystems.com>
2020-01-23 19:13:40 +0530

    fixed ui for faq

--- src/components/static/how_works/ask_question.js
+++ src/components/static/how_works/ask_question.js
@@ -12,24 +12,24 @@ export default class SellerOverview extends Component{
         <h3 className="font-darkred">Ask us question</h3>
         <div className="col-md-6 offset-md-3">	
 	        <form className="ask_form mb-5">
-	        	<div class="form-group">
+	        	<div className="form-group">
 					    <label for="exampleInputEmail1">Name</label>
-					    <input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"/>
+					    <input type="text" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"/>
 					  </div>
-					  <div class="form-group">
+					  <div className="form-group">
 					    <label for="exampleInputEmail1">Email</label>
-					    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"/>
+					    <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"/>
 					  </div>
-					  <div class="form-group">
+					  <div className="form-group">
 					    <label for="exampleInputEmail1">Phone</label>
-					    <input type="number" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"/>
+					    <input type="number" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"/>
 					  </div>
-					  <div class="form-group">
+					  <div className="form-group">
 					    <label for="exampleInputPassword1">Ask a Question?</label>
-					    <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+					    <textarea className="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
 					  </div>
 					  <div className="col-md-12 text-center">
-					  	<button type="submit" class="btn red-btn">Submit</button>
+					  	<button type="submit" className="btn red-btn">Submit</button>
 					  </div>
 					</form>
 				</div>
