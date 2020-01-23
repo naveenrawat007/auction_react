bd75ec20c4fb0f970d259d024d1a83b47c58f464
rajan-gith <rajan.prajapati@berylsystems.com>
2020-01-23 15:36:48 +0530

    top bar changes

--- src/components/static/topbar.js
+++ src/components/static/topbar.js
@@ -1,7 +1,7 @@
 import React, {Component} from 'react';
 import {Link} from 'react-router-dom';
 import HelpSidebar from './help/help_sidebar.js';
-
+import HowWorksSidebar from './how_works/how_works_sidebar.js';
 
 export default class TopSidebar extends Component{
   _isMounted = false
@@ -33,6 +33,11 @@ export default class TopSidebar extends Component{
         return <HelpSidebar path="qualify_deal"/>;
       case 'top_reason_help':
         return <HelpSidebar path="top_reason"/>;
+      case 'seller_how_works':
+        return <HowWorksSidebar path="seller_how_works"/>;
+      case 'buyer_how_works':
+        return <HowWorksSidebar path="buyer_how_works"/>;
+
       default:
     }
   }
@@ -46,7 +51,7 @@ export default class TopSidebar extends Component{
           <div className="col-md-12 main_help px-0">
             <ul className="nav nav-tabs help_tabs" id="myTab" role="tablist">
               <li className="nav-item">
-                <a className="nav-link" id="works_tab" data-toggle="tab" href="#works_tab" role="tab" aria-controls="works_tab" aria-selected="true">How Everything Works</a>
+                <Link className={this.checkActive("how_works")} id="works_tab" data-toggle="tab" to="/how-everything-works/seller" role="tab" aria-controls="works_tab" aria-selected="true">How Everything Works</Link>
               </li>
               <li className="nav-item">
                 <a className="nav-link" id="relator_portal" data-toggle="tab" href="#relator_portal" role="tab" aria-controls="relator_portal" aria-selected="false">Relator Portal</a>
