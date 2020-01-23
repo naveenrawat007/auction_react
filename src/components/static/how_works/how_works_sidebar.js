ae4ea42188844533b810dfea8e6e483b54ebeb7f
rajan-gith <rajan.prajapati@berylsystems.com>
2020-01-23 15:38:19 +0530

    how works components added

---
+++ src/components/static/how_works/how_works_sidebar.js
@@ -0,0 +1,71 @@
+import React, {Component} from 'react';
+import {Link} from 'react-router-dom';
+import SellerOverview from './seller.js';
+import BuyerOverview from './buyer.js';
+
+export default class HowWorksSidebar extends Component{
+  _isMounted = false
+  constructor(props) {
+    super(props);
+    this.state = {
+      path: props.path
+    }
+  }
+  componentDidMount () {
+    window.scroll(0,0)
+  }
+  checkActive = (current_path) => {
+    if (this.state.path === (current_path)){
+      return "nav-link active"
+    }else {
+      return "nav-link";
+    }
+  }
+  renderSwitch = () => {
+    switch (this.state.path) {
+      case 'buyer_how_works':
+        return <BuyerOverview/>;
+      case 'seller_how_works':
+        return <SellerOverview/>;
+      default:
+    }
+  }
+  render(){
+    return(
+      <div class="tab-content" id="myTabContent">
+        <div class="tab-pane show active" id="helpful_info" role="tabpanel" aria-labelledby="helpful_info">
+          <div className="help_us row mx-0">
+            <div className="col-md-3 px-0">
+              <ul className="nav nav-tabs flex-column" id="myTab" role="tablist">
+                <li className="nav-item">
+                  <Link to="/how-everything-works/seller" className={this.checkActive("seller_how_works")} >Seller Overview</Link>
+                  {/* <a className="nav-link active" id="deal-qualify" data-toggle="tab" href="#deal-qualify" role="tab" aria-controls="deal-qualify" aria-selected="true">Does your Deal qualify to Auction?</a> */}
+                </li>
+                <li className="nav-item">
+                  <Link className={this.checkActive("buyer_how_works")} id="upper-deals" data-toggle="tab" to="/how-everything-works/buyer" role="tab" aria-controls="upper-deals" aria-selected="false">Buyer Overview</Link>
+                </li>
+                <li className="nav-item">
+                  <Link className={this.checkActive("landlord_analyzer")} id="landlord-analyzer" data-toggle="tab" to="#" role="tab" aria-controls="landlord-analyzer" aria-selected="false">Hardmoney</Link>
+                </li>
+                <li className="nav-item">
+                  <Link className={this.checkActive("confident_deal")} id="deal-analysis" data-toggle="tab" to="#" role="tab" aria-controls="deal-analysis" aria-selected="false">Resources</Link>
+                </li>
+                <li className="nav-item">
+                  <Link className="nav-link" id="deal-week" data-toggle="tab" to="#" role="tab" aria-controls="deal-week" aria-selected="false">Ask us Question</Link>
+                </li>
+                <li className="nav-item">
+                  <Link className="nav-link" id="about-us" data-toggle="tab" to="/about" role="tab" aria-controls="about-us" aria-selected="false">About Us</Link>
+                </li>
+              </ul>
+            </div>
+            <div className="col-md-9 px-0">
+              <div className="tab-content" id="myTabContent">
+                {this.renderSwitch()}
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
---
+++ src/components/static/how_works/seller.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+
+export default class SellerOverview extends Component{
+  _isMounted = false
+  render(){
+    return(
+      <>
+      </>
+    )
+  }
+}
