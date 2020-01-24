f0fd04bf20e005825f070adb2290022242123636
rajan-gith <rajan.prajapati@berylsystems.com>
2020-01-24 14:48:59 +0530

    format time added

--- public/function.js
+++ public/function.js
@@ -24,7 +24,26 @@ function formatDate(date) {
     var day = date.getDate();
     var monthIndex = date.getMonth();
     var year = date.getFullYear();
-    return day + ' ' + monthNames[monthIndex] + ' ' + year;
+    return  monthNames[monthIndex]+ " " + day + ', ' + year;
+  }
+  else {
+    return "";
+  }
+}
+function formatTime(time) {
+  if (time !== ""){
+    time = new Date(time)
+    var hour = time.getHours();
+    var minute = time.getMinutes();
+    if (minute < 10) {
+      minute = "0" + minute;
+    }
+    var ampm = "am";
+    if( hour > 12 ) {
+      hour -= 12;
+      ampm = "pm";
+    }
+    return hour + ":" + minute + " " +ampm
   }
   else {
     return "";
