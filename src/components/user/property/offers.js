0dde59e2fbd232056e288e6e496c2190ef5f3571
rajan-gith <rajan.prajapati@berylsystems.com>
2020-02-19 19:51:58 +0530

    search handler updated

--- src/components/user/property/offers.js
+++ src/components/user/property/offers.js
@@ -110,6 +110,7 @@ export default class ListOfferProperty extends Component{
     const{ name, value } = event.target;
     this.setState({
       isLoaded: false,
+      page: 1,
       [name]: value
     }, function functionName() {
       clearTimeout(this.getPropertiesListTimeout);
