f6d9adbbfecc8ba9b8f59c11738e6e64e9441cc8
rajan-gith <rajan.prajapati@berylsystems.com>
2020-02-24 14:50:39 +0530

    new static help page component added

---
+++ src/components/static_help/help_and_faq.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+import {Link} from 'react-router-dom';
+
+export default class HelpAndFaq extends Component{
+  _isMounted = false
+  componentDidMount(){
+    window.scroll(0,0)
+  }
+  render(){
+    return(
+    	<div className="">
+      </div>
+    )
+  }
+}
