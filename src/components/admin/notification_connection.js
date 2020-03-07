cabeab1fba471a8fcbf5080e92c03918128a9d53
Rajan Prajapati <rajan.prajapati@berylsystems.com>
2020-03-07 15:48:47 +0530

    admin notification js added

---
+++ src/components/admin/notification_connection.js
@@ -0,0 +1,34 @@
+import ActionCable from 'actioncable'
+
+function NotificationConnection(callback) {
+  let access_token = localStorage.getItem('auction_admin_token')
+
+  var wsUrl = 'ws://' + process.env.REACT_APP_BACKEND_WS_URL + '/cable'
+  wsUrl += '?access-token=' + access_token
+  this.callback = callback
+
+  this.connection = ActionCable.createConsumer(wsUrl)
+  this.roomConnections = []
+}
+
+NotificationConnection.prototype.disconnect = function() {
+  this.roomConnections.forEach(c => c.conn.consumer.connection.close())
+}
+
+NotificationConnection.prototype.createRoomConnection = function() {
+  var scope = this
+  return this.connection.subscriptions.create({channel: 'NotificationChannel'}, {
+    connected: function() {
+      console.log('connected to NotificationChannel.')
+    },
+    disconnected: function() {
+      console.log("disconnect");
+    },
+    received: function(data) {
+      return scope.callback(data)
+    },
+  })
+}
+
+
+export default NotificationConnection
