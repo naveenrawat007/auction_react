a0ce81cde3e0543562da1824eedd000ddc91574c
lalit Kumar <lalit.Kumar@berylsystems.com>
2020-04-04 11:48:32 +0530

    fixed ui for billing tab in profile page

--- src/components/user/property/property_three.js
+++ src/components/user/property/property_three.js
@@ -79,120 +79,48 @@ export default class PropertyShowThree extends Component {
             </div>
             <div className="col-md-12 py-3">
               <div className="bg_white">
+                <div className="register_bid_description py-3 px-5">
+                  <h3 className="mb-1">Total Amount Bid: $375,000</h3>
+                  <p className="mb-0">Buyer will close this property: April 30, 2020</p>
+                </div>
+              </div>
+            </div>
+            <div className="col-md-12 py-3">
+              <div className="bg_white">
                 <div className="register_bid_form py-3 px-5">
                   <form>
                     <div className="register_bid_title mb-2 col-md-8">
-                      <h4>Place Your Bid</h4>
-                    </div>
-                    <div className="form-group row mx-0 mb-0">
-                      <label for="staticEmail" className="col-sm-3 col-form-label text-right">Your amount offer is&nbsp;&nbsp;:</label>
-                      <div class="input-group col-md-3">
-                        <div class="input-group-prepend">
-                          <span class="input-group-text group-box"><FontAwesomeIcon icon={faMinus}/></span>
-                        </div>
-                        <input type="text" class="form-control" aria-label="Amount (to the nearest dollar)"/>
-                        <div class="input-group-append">
-                          <span class="input-group-text group-box"><FontAwesomeIcon icon={faPlus}/></span>
-                        </div>
+                      <h4 className="mb-2">A. Register to Bid</h4>
+                      <div className="font-blue-bold px-3">
+                        <p className="mb-0">Tina</p>
+                        <p className="mb-0">Nguyen</p>
+                        <p className="mb-0">Tinaaaa@gmail.com</p>
+                        <p className="mb-0">777-1111-2222</p>
                       </div>
                     </div>
-                    <div className="form-group row mx-0 align-items-center mb-0">
-                      <label for="inputPassword" className="col-sm-3 col-form-label text-right">Internet Transaction Fee&nbsp;&nbsp;:</label>
-                      <div className="col-sm-3 text-right font-weight-bold">
-                        <p className="values_input">$97</p>
+                    <div className="register_bid_title mb-2 col-md-8">
+                      <h4>B. Are you buying this property for yourself? <span className="font-blue-bold">NO</span></h4>
+                      <div className="col-md-10 relator_info">
+                        <h5>Relator Information</h5>
+                        <p className="mb-2">There will be no fee or comission paid by AuctionMyDeal.com or any seller unless they are listed on the MLS and then you will recieve the comission offered on MLS by the sponsoring broker.</p>
+                      </div>
+                      <div className="font-blue-bold px-3">
+                        <p className="mb-0">Richard</p>
+                        <p className="mb-0">Wall</p>
+                        <p className="mb-0">#0453989</p>
+                        <p className="mb-0">Richar Wall Realtors</p>
+                        <p className="mb-0">Richard@richardwall.com</p>
+                        <p className="mb-0">777-1111-2222</p>
                       </div>
                     </div>
-                    <div className="form-group row mx-0 align-items-center">
-                      <label for="staticEmail" className="col-sm-3 col-form-label text-right">Total Due&nbsp;&nbsp;:</label>
-                      <div className="col-sm-3 text-center font-weight-bold">
-                        <p className="values_input values_input_border">$378,097</p>
-                      </div>
+                    <div className="register_bid_title mb-2 col-md-8">
+                      <h4 className="mb-2">C. I want to purchase the property as: <span className="font-blue-bold">Bussiness</span></h4>
+                      <p className="font-blue-bold px-3">Angle Investor LLC</p>
                     </div>
-                    <div className="form-group row mx-0">
-                      <label for="inputPassword" className="col-sm-3 col-form-label text-right">Enter Promo Code&nbsp;&nbsp;:</label>
-                      <div className="col-sm-3">
-                        <input type="text" className="form-control"/>
-                      </div>
+                    <div className="register_bid_title mb-2 col-md-8">
+                      <h4>D. Proof of funds and/or Preapproval Letter: <span className="font-blue-bold">Cash</span></h4>
                     </div>
-                    <div className="col-md-8 warning_alert p-2 d-flex align-items-center justify-content-start">
-                      <FontAwesomeIcon icon={faExclamationCircle}/>
-                      <p>if your offer is accepted then the seller is requesting a deposit in the amount of $3,00.</p>
-                    </div>
-                    <p className="seller_request">Seller is requesting that you close by:</p>
-                    <div className="form-group row mx-0">
-                      <label for="inputPassword" className="col-sm-5 col-form-label text-left">How soon can you close on this property&nbsp;&nbsp;:</label>
-                      <div className="col-sm-3">
-                        <input type="text" className="form-control"/>
-                      </div>
-                    </div>
-                    <div className="form-group row mx-0 align-items-center">
-                      <label for="inputPassword" className="col-sm-5 col-form-label text-left">If my bid is not initially accepted by the seller then please hold my bid as backup offer for&nbsp;&nbsp;:</label>
-                      <div className="col-sm-3">
-                        <select className="form-control">
-                          <option>7 Days</option>
-                          <option>14 Days</option>
-                        </select>
-                      </div>
-                    </div>
-                    <div className="register_bid_title mb-2 col-md-8 d-flex align-items-center justify-content-between">
-                      <h4>Payment Information</h4>
-                    </div>
-                    <div className="form-group row mx-0">
-                      <label for="staticEmail" className="col-sm-2 col-form-label text-right">Card Number&nbsp;&nbsp;:</label>
-                      <div className="col-sm-6">
-                        <input type="text" className="form-control"/>
-                      </div>
-                    </div>
-                    <div className="form-group row mx-0">
-                      <label for="staticEmail" className="col-sm-2 col-form-label text-right">Expiry Date&nbsp;&nbsp;:</label>
-                      <div className="col-sm-6">
-                        <input type="text" className="form-control"/>
-                      </div>
-                    </div>
-                    <div className="form-group row mx-0">
-                      <label for="inputPassword" className="col-sm-2 col-form-label text-right">CVV&nbsp;&nbsp;:</label>
-                      <div className="col-sm-6">
-                        <input type="text" className="form-control"/>
-                      </div>
-                    </div>
-                    <div className="col-md-8 warning_alert p-2 d-flex align-items-center justify-content-start">
-                      <FontAwesomeIcon icon={faExclamationCircle}/>
-                      <p><b>$97 Bid Deposit</b>(a hold on your credit card) will be assessed once you submit an offer or bid to the site.</p>
-                    </div>
-                    <p className="seller_request">Check the boxes to confirm the following:</p>
-                    <div className="form-group form-check">
-                      <input type="checkbox" className="form-check-input" id="exampleCheck1" />
-                      <label className="form-check-label" for="exampleCheck1">I understand and agree that the seller reserves the right to refuse any bid, highest or otherwise and final acceptance of a selected bid is expressly subject to the sellers signature on the purchase and sale agreement</label>
-                    </div>
-                    <div className="form-group form-check">
-                      <input type="checkbox" className="form-check-input" id="exampleCheck1" />
-                      <label className="form-check-label" for="exampleCheck1">I agree to buy this property As-is, where is with all faults.</label>
-                    </div>
-                    <div className="form-group form-check">
-                      <input type="checkbox" className="form-check-input" id="exampleCheck1" />
-                      <label className="form-check-label" for="exampleCheck1">I understand that the pictures, video ARV proof and rehab numbers are provided for informational purposes only and I have done my own due dilligence for this property i am bidding on.</label>
-                    </div>
-                    <div className="form-group form-check">
-                      <input type="checkbox" className="form-check-input" id="exampleCheck1" />
-                      <label className="form-check-label" for="exampleCheck1">I agree to sign and return the purchase documents within 1 bussiness day of receipt or my offer could be rejected.</label>
-                    </div>
-                    <div className="form-group form-check">
-                      <input type="checkbox" className="form-check-input" id="exampleCheck1" />
-                      <label className="form-check-label" for="exampleCheck1">I agree to deliver(whatever seller enters as required earnest money $) as nonrefundable earnest money to title company on Executed Contract if i am the winning bidder within the 48 bussiness hours or my winning bidder status can be cancelled.</label>
-                    </div>
-                    <div className="form-group form-check">
-                      <input type="checkbox" className="form-check-input" id="exampleCheck1" />
-                      <label className="form-check-label" for="exampleCheck1">I agree to respond to AuctionMyDeal.com inquires within 1 bussiness day of receipt.</label>
-                    </div>
-                    <div className="form-group form-check">
-                      <input type="checkbox" className="form-check-input" id="exampleCheck1" />
-                      <label className="form-check-label" for="exampleCheck1">I (Buyers) agrees to pay for all standaard buyer and seller closing cost including title policy. Seller will pay to remove all liens, taxes and HOA dues owed and prorated up until the day of closing.</label>
-                    </div>
-                    <div className="form-group form-check">
-                      <input type="checkbox" className="form-check-input" id="exampleCheck1" />
-                      <label className="form-check-label" for="exampleCheck1">I am the prospective buyer or an authorized representative of the prospective buyer for this transaction. I represent that on my own behalf and behalf of any prospective buyer I represent. I have read and agree with the participation terms for making this offer, and agree to AuctionMyDeal.com Terms and Conditions , Privacy Policy and any special terms that may apply.</label>
-                    </div>
-                    <div className="col-md-12 text-center">
+                    <div className="col-md-12 mt-4 text-center">
                       <button class="btn red-btn" type="submit">Submit</button>
                     </div>
                   </form>
